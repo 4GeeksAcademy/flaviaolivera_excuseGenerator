d56d6e6811990b11ea9a4589dfa0956103df2539
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-09 23:23:40 +0000

    Funcion creada para generar excusas. Elemento temporal conectado a html

--- src/app.js
+++ src/app.js
@@ -3,5 +3,22 @@ import "./style.css";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = who[Math.floor(Math.random() * who.length)];
+  let accion = action[Math.floor(Math.random() * action.length)];
+  let que = what[Math.floor(Math.random() * what.length)];
+  let cuando = when[Math.floor(Math.random() * when.length)];
+  let excusa = quien + " " + accion + " " + " " + que + " " + cuando;
+
+  document.querySelector("#excuse").innerHTML = excusa;
 };
